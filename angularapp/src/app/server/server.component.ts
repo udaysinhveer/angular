5b1f5db25912cabdabcb811824ef2ea12f18bb2f
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-11-30 09:29:54 +0530

    created servers component
    created servers component using angular CLI commands

---
+++ angularapp/src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'app-server',
+    template : `<h1> this is my server component 
+     </h1> <h2>this is h2 </h2>`,              // use backtick instead of ''. 
+    styleUrls : ['./server.component.css']
+ 
+})
+
+export class ServerComponent{
+
+}
