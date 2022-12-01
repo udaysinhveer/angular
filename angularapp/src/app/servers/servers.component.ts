95fbe637e663c2084199e1eb12cc9f2ef701df10
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-01 16:09:59 +0530

    Servers Component

--- angularapp/src/app/servers/servers.component.ts
+++ angularapp/src/app/servers/servers.component.ts
@@ -6,8 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  name : string =  "Udaysinh Veer";
+  ServerId: number = 27;
+  ServerStatus : string = 'Online';
+  serverCurrentStatus : string = 'Offline';
+  allowNewFeature = true;  // used this property -  for Property Binding
+  imgUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png?20221110153201';
 
-  constructor() { }
+  serverCreationStatus : string = "Server Not Created"
+  serverName : string = 'Udaysinhveer.com';
+
+  constructor() {
+    setTimeout(()=> {                // we set this is time to enable button
+      this.allowNewFeature = false    // after 4 second button will be enable. 
+    }, 4000)
+    
+   }
+
+  getServerStatus(){
+    return this.serverCurrentStatus;  // we create method for string interpolation
+  };
+
+  onCreateServer(){
+    return this.serverCreationStatus = "Server Created"   // this method is for Event Binding
+  }
 
   ngOnInit() {
   }
